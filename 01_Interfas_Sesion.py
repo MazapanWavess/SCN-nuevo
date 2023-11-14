f013851188493cb9713e5ea3580cba46ea348f87
MazapanWavess <emanuelrrz1234@gmail.com>
2023-11-13 19:07:36 -0600

    a

--- 01_Interfas_Sesion.py
+++ 01_Interfas_Sesion.py
@@ -1,18 +1,20 @@
 # IMPORTAMOS LIBRERIAS (FALTA LA LIBRERIA SQLITE)
 import tkinter
 import sqlite3 
+from PIL import Image, ImageTk
+
 
 #--------------------------------------------------------
 # AQUI SE PONDRA LAS FUNCIONES QUE OCUPAREMOS
 
 # FUNCIONES PARA BOTON INICIA SESION
 def entrar(e):
-    boton_Iniciar["background"] = "#00ADB5"
-    boton_Iniciar["foreground"] = "#222831"
+    boton_Iniciar["background"] = "#2d3541"
+    boton_Iniciar["foreground"] = "#00ADB5"
 
 def salir(e):
-    boton_Iniciar["background"] = "#222831"
-    boton_Iniciar["foreground"] = "#00ADB5"
+    boton_Iniciar["background"] = "#00ADB5"
+    boton_Iniciar["foreground"] = "#2d3541"
 
 # FUNCIONES PARA LAS CAJAS DE TEXTO
 def on_enter_usuario(e):
@@ -37,30 +39,41 @@ def on_leave_contraseña(e):
 ventana = tkinter.Tk()
 ventana.geometry("925x500")
 ventana.title("SCN")
-ventana.config(bg='white')
+ventana.config(bg='#222831')
 
 #--------------------------------------------------------
 # AQUI SE PONDRA LA TABLAS PARA LA BASE DE DATOS
 
+
 #--------------------------------------------------------
 # Aquí se debe poner la imagen
+
+signin_win=tkinter.Frame(ventana,width=925,height=500,bg='#222831')
+signin_win.place(x=0,y=0)
+f1=tkinter.Frame(signin_win,width=350,height=350,bg='#222831')
+f1.place(x=480,y=100)
+
+global img1
+img1 = ImageTk.PhotoImage(Image.open("F:\\04_Proyecto\\scn nuevo\\SCN-nuevo\\01.png"))
+tkinter.Label(signin_win,image=img1,border=0,bg='#222831').place(x=50,y=50)
+
 
 #--------------------------------------------------------
 # ETIQUETAS Y CAJAS DE TEXTO INICIO DE SESION
 
 # ETIQUETA 1
-l2 = tkinter.Label(ventana, text="Inicia sesión", fg='#00ADB5', bg='white')
+l2 = tkinter.Label(ventana, text="Inicia sesión", fg='#00ADB5', bg='#222831')
 l2.config(font=('Microsoft YaHei UI Light', 24, 'bold'))
 l2.place(x=570, y=60)
 
 # FRAME PARA LAS CAJAS DE TEXTO
-f1 = tkinter.Frame(ventana, width=350, height=350, bg='white')
+f1 = tkinter.Frame(ventana, width=350, height=350, bg='#222831')
 f1.place(x=480, y=100)
 
 
 
 # CAJA 1 CON SUS FUNCIONES
-usuario = tkinter.Entry(f1, width=25, fg='#00ADB5', border=0, bg='white')
+usuario = tkinter.Entry(f1, width=25, fg='#00ADB5', border=0, bg='#222831')
 usuario.config(font=('Microsoft YaHei UI Light', 11, ))
 usuario.bind("<FocusIn>", on_enter_usuario)
 usuario.bind("<FocusOut>", on_leave_usuario)
@@ -68,12 +81,12 @@ usuario.insert(0, 'Usuario')
 usuario.place(x=30, y=60)
 
 # frame de la parte inferior de la caja de texto 1
-tkinter.Frame(f1, width=295, height=2, bg='#222831').place(x=25, y=87)
+tkinter.Frame(f1, width=295, height=2, bg='WHITE').place(x=25, y=87)
 
 
 
 # CAJA DE TEXTO 2 CON SUS FUNCIONES
-contraseña = tkinter.Entry(f1, width=21, fg='#00ADB5', border=0, bg='white')
+contraseña = tkinter.Entry(f1, width=21, fg='#00ADB5', border=0, bg='#222831')
 contraseña.config(font=('Microsoft YaHei UI Light', 11, ))
 contraseña.bind("<FocusIn>", on_enter_contraseña)
 contraseña.bind("<FocusOut>", on_leave_contraseña)
@@ -81,17 +94,17 @@ contraseña.insert(0, 'Contraseña')
 contraseña.place(x=30, y=130)
 
 # frame de la parte inferior de la caja de texto 2
-tkinter.Frame(f1, width=295, height=2, bg='#222831').place(x=25, y=157)
+tkinter.Frame(f1, width=295, height=2, bg='white').place(x=25, y=157)
 
 #--------------------------------------------------------
 # CREAMOS EL BOTON PARA EL INICIO DE SESION
 
 boton_Iniciar = tkinter.Button(f1, width=39, pady=7, text='Inicia sesión',
                                 bg="#00ADB5",
-                                fg="#222831",
+                                fg="#2d3541",
                                 border=0,
-                                activeforeground="#222831",
-                                activebackground="#00ADB5")
+                                activeforeground="#00ADB5",
+                                activebackground="#2d3541")
 
 boton_Iniciar.place(x=35, y=204)
 boton_Iniciar.bind("<Enter>", entrar)
@@ -102,11 +115,11 @@ boton_Iniciar.bind("<Leave>", salir)
 
 #ETIQUETA SI NO TIENE CUENTA
 
-l1=tkinter.Label(f1,text="No tienes cuenta?",fg="#222831",bg='white')
+l1=tkinter.Label(f1,text="No tienes cuenta?",fg="white",bg='#222831')
 l1.config(font=('Microsoft YaHei UI Light',9, ))
-l1.place(x=95,y=250) 
+l1.place(x=95,y=250)
 
-b2=tkinter.Button(f1,width=8,text='Registrate',border=0,bg='white',fg='#00ADB5')
+b2=tkinter.Button(f1,width=8,text='Registrate',border=0,bg='#222831',fg='#00ADB5')
 b2.place(x=200,y=250)
 
 
